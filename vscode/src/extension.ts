f8f0fbd018d3714820577ba89f28b492856fd46c
Lau Skorstengaard <lau@toitware.com>
2021-05-21 09:51:02 +0200

    Revert "Remove commands from LSP client"
    This reverts commit e028849117ac6e371115476b00a7f74e863cb7b1.

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -1,17 +1,17 @@
 "use strict";
 
-import { commands as Commands, ExtensionContext, window as Window } from "vscode";
-import { activateLsp, deactivateLsp } from "./lspClient";
-import { createDeployCommand, createRunCommand } from "./toitExec";
+import { commands as Commands, ExtensionContext, window as Window } from 'vscode';
+import { activateLsp, deactivateLsp } from './lspClient';
+import { createDeployCommand, createRunCommand } from './toitExec';
 
-export function activate (context: ExtensionContext): void {
-  const toitOutput = Window.createOutputChannel("Toit");
-  context.subscriptions.push(Commands.registerCommand("toit.devRun", createRunCommand(toitOutput)));
-  context.subscriptions.push(Commands.registerCommand("toit.devDeploy", createDeployCommand(toitOutput)));
+export function activate(context: ExtensionContext) {
+  let toitOutput = Window.createOutputChannel('Toit');
+  context.subscriptions.push(Commands.registerCommand('toit.devRun', createRunCommand(toitOutput)));
+  context.subscriptions.push(Commands.registerCommand('toit.devDeploy', createDeployCommand(toitOutput)));
 
   activateLsp(context);
 }
 
-export function deactivate (): Thenable<void> {
+export function deactivate(): Thenable<void> {
   return deactivateLsp();
 }
