9a5406993a0c6d22af0a3b950775e663e99f1ea8
Lau Skorstengaard <lau@toitware.com>
2021-05-27 13:08:43 +0200

    Add messages to auth command

--- vscode/src/toitAuth.ts
+++ vscode/src/toitAuth.ts
@@ -1,9 +1,18 @@
 "use strict";
 
+import { window as Window } from "vscode";
 import { CommandContext, ensureAuth } from "./utils";
 
+async function ensureAuthCommand(cmdContext: CommandContext): Promise<void> {
+  try {
+    await ensureAuth(cmdContext);
+  } catch(e) {
+    Window.showErrorMessage(`Login to toit.io failed: ${e.message}.`);
+    return;
+  }
+  Window.showInformationMessage(`Authenticated with toit.io.`);
+}
+
 export function createEnsureAuth(cmdContext: CommandContext): () => void {
-  return () => {
-    ensureAuth(cmdContext);
-  };
+  return () => ensureAuthCommand(cmdContext);
 }
