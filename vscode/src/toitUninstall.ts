2105e5fabfd1f15002df531e846ccc9a8423a886
Lau <5364453+lask@users.noreply.github.com>
2021-06-04 11:59:08 +0200

    Add app uninstall button (#37)
    * Add categories to commands
    
    * Change the command visibility
    
    * Add icon to devices
    
    * Add icons
    
    * Add list apps
    
    * Add Apps to device view
    
    * Add tooltip
    
    * Move lines
    
    * Update output
    
    * Remove device info from tree view
    
    The info has been moved to the tooltip
    
    * Add app icon
    
    * Add uninstall
    
    * Remove TODO
    
    * Disable command in palette
    
    * Use context to update device view

---
+++ vscode/src/toitUninstall.ts
@@ -0,0 +1,12 @@
+"use strict";
+
+import cp = require("child_process");
+import { App, RelatedApp } from "./app";
+import { CommandContext, uninstallApp } from "./utils";
+
+export function createUninstallCommand(cmdContext: CommandContext): (app: App) => void {
+  return async (app: RelatedApp) => {
+    await uninstallApp(cmdContext, app.app());
+    cmdContext.refreshDeviceView();
+  };
+}
