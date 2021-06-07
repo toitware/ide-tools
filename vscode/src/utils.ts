cacf37b2da8e4bf9a62947e3dbb75a09537ed8c8
Lau <5364453+lask@users.noreply.github.com>
2021-06-07 08:28:30 +0200

    Add toit statusbar item (#39)

--- vscode/src/utils.ts
+++ vscode/src/utils.ts
@@ -243,3 +243,9 @@ function preferElement<T>(index: number, list: T[]): void {
 export async function uninstallApp(ctx: CommandContext, app: App) {
   await execFile(ctx.toitExec, [ "dev", "-d", app.deviceID, "uninstall", app.jobID ]);
 }
+
+export async function getOrganization(ctx: CommandContext) {
+  await ensureAuth(ctx);
+  const { stdout } = await execFile(ctx.toitExec, [ "auth", "get-organization" ]);
+  return stdout.slice(13);
+}
