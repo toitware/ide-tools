a1ede94767a610f1d29688f134accf0211365c19
Lau Skorstengaard <lau@toitware.com>
2021-06-09 08:57:03 +0200

    Add local device variable

--- vscode/src/treeView.ts
+++ vscode/src/treeView.ts
@@ -71,36 +71,37 @@ class DeviceTreeRoot extends DeviceTreeItem {
   }
 
   treeItem(): TreeItem {
-    const contextValue = this.device().isSimulator ? "device-simulator" : "device";
-    const label = `${this.device().name}`;
-    const p = this.device().isActive ? DeviceTreeRoot.activeIcons : DeviceTreeRoot.inactiveIcons;
+    const device = this.device();
+    const contextValue = device.isSimulator ? "device-simulator" : "device";
+    const label = device.name;
+    const icons = device.isActive ? DeviceTreeRoot.activeIcons : DeviceTreeRoot.inactiveIcons;
     const tooltipMarkdown =
 `
-### ${this.device().name} ${this.device().isSimulator ? "(simulator)" : ""}
+### ${device.name} ${device.isSimulator ? "(simulator)" : ""}
 
 --------------------------------
 #### Device ID
 
-${this.device().deviceID}
+${device.deviceID}
 
 --------------------------------
 #### Firmware
 
-${this.device().runningFirmware} ${this.device().configureFirmware ? `\u279f ${this.device().configureFirmware}` : ""}
+${device.runningFirmware} ${device.configureFirmware ? `\u279f ${device.configureFirmware}` : ""}
 
 --------------------------------
 #### Last seen
 
-${new Date(this.device().lastSeen).toLocaleTimeString(undefined)}
+${new Date(device.lastSeen).toLocaleTimeString(undefined)}
 
-${new Date(this.device().lastSeen).toLocaleDateString(undefined, {weekday: "long", year: "numeric", month: "long", day: "numeric"})}
+${new Date(device.lastSeen).toLocaleDateString(undefined, {weekday: "long", year: "numeric", month: "long", day: "numeric"})}
 `
     return new class extends TreeItem {
       constructor() {
         super(label, TreeItemCollapsibleState.Collapsed);
       }
       contextValue = contextValue;
-      iconPath = p;
+      iconPath = icons;
       tooltip = new MarkdownString(tooltipMarkdown);
     }();
   }
