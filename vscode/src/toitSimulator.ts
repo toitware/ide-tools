06125af2801488a753f58cf498484328d75c49c4
Lau Skorstengaard <lau@toitware.com>
2021-06-11 10:04:36 +0200

    Improve simulator stop refresh

--- vscode/src/toitSimulator.ts
+++ vscode/src/toitSimulator.ts
@@ -1,7 +1,9 @@
 import cp = require("child_process");
+import { promisify } from "util";
 import { OutputChannel, window as Window } from "vscode";
-import { Device, RelatedDevice } from "./device";
-import { CommandContext, ensureAuth, selectDevice } from "./utils";
+import { Device } from "./device";
+import { CommandContext, ensureAuth, getToitPath, selectDevice } from "./utils";
+const execFile = promisify(cp.execFile);
 
 async function executeStopCommand(ctx: CommandContext, device?: Device) {
   try {
@@ -14,16 +16,16 @@ async function executeStopCommand(ctx: CommandContext, device?: Device) {
     if (!device) device = await selectDevice(ctx, {"activeOnly": false, "simulatorOnly": true});
 
     if (!device.isSimulator) return Window.showErrorMessage("Non-simulator selected.");
-    cp.spawn("toit", [ "simulator", "stop", device.deviceID ]);
-    ctx.refreshDeviceView();
+    await execFile(getToitPath(), [ "simulator", "stop", device.deviceID ]);
+    ctx.refreshDeviceView(device);
   } catch (e) {
     Window.showErrorMessage(`Stop simulator failed: ${e.message}`);
   }
 }
 
 export function createStopSimCommand(cmdContext: CommandContext): () => void {
-  return (dev?: RelatedDevice) => {
-    executeStopCommand(cmdContext, dev?.device());
+  return (dev?: Device) => {
+    executeStopCommand(cmdContext, dev);
   };
 }
 
@@ -44,7 +46,7 @@ async function executeStartCommand(ctx: CommandContext) {
       args.push("--alias");
       args.push(name);
     }
-    const commandProcess = cp.spawn("toit", args);
+    const commandProcess = cp.spawn(getToitPath(), args);
     const toitOutput: OutputChannel = ctx.toitOutput();
     toitOutput.show();
     commandProcess.stdout.on("data", data => toitOutput.append(`${data}`));
