4ee1fe586502260ccc94e71d85f7d11a999b18f1
Lau Skorstengaard <lau@toitware.com>
2021-06-11 10:29:05 +0200

    Use getToitPath

--- vscode/src/toitExec.ts
+++ vscode/src/toitExec.ts
@@ -6,7 +6,7 @@ import { promisify } from "util";
 import { OutputChannel, window as Window } from "vscode";
 import { Device } from "./device";
 import { } from "./treeView";
-import { CommandContext, ensureAuth, selectDevice } from "./utils";
+import { CommandContext, ensureAuth, getToitPath, selectDevice } from "./utils";
 import cp = require("child_process");
 const execFile = promisify(cp.execFile);
 
@@ -41,7 +41,7 @@ async function executeRunCommand(ctx: CommandContext, device?: Device) {
   try {
     if (!device) device = await selectDevice(ctx, { "activeOnly": true, "simulatorOnly": false });
 
-    const commandProcess = cp.spawn("toit", [ "dev", "-d", device.name, "run", filePath ]);
+    const commandProcess = cp.spawn(getToitPath(), [ "dev", "-d", device.name, "run", filePath ]);
     const toitOutput: OutputChannel = ctx.outputChannel(device.deviceID, device.name);
     toitOutput.show();
     commandProcess.stdout.on("data", data => toitOutput.append(`${data}`));
@@ -69,7 +69,7 @@ async function executeDeployCommand(ctx: CommandContext, device?: Device) {
   try {
     if (!device) device = await selectDevice(ctx, { "activeOnly": false, "simulatorOnly": false });
 
-    const { stdout, stderr } = await execFile("toit", [ "dev", "-d", device.name, "deploy", filePath ]);
+    const { stdout, stderr } = await execFile(getToitPath(), [ "dev", "-d", device.name, "deploy", filePath ]);
     const toitOutput: OutputChannel = ctx.outputChannel(device.deviceID, device.name);
     toitOutput.show();
     toitOutput.append(stdout);
