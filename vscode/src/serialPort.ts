94b592440b3007654c4d2a94f35507a69028da27
Lau Skorstengaard <lau@toitware.com>
2021-06-14 17:14:06 +0200

    Add name to serial port

--- vscode/src/serialPort.ts
+++ vscode/src/serialPort.ts
@@ -2,7 +2,9 @@ import { ThemeIcon, TreeItem, TreeItemCollapsibleState } from "vscode";
 
 export class SerialPort extends TreeItem {
   iconPath = new ThemeIcon("plug");
+  name: string;
   constructor(port: string) {
     super(port, TreeItemCollapsibleState.None);
+    this.name = port;
   }
 }
