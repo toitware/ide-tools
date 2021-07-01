f08b0da59b87a2de79b2177ad96ed81543970af1
Lau <5364453+lask@users.noreply.github.com>
2021-07-01 12:59:17 +0200

    Check for the toit CLI executable at extension start (#115)

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -2,7 +2,7 @@
 // Use of this source code is governed by an MIT-style license that can be
 // found in the LICENSE file.
 
-import { commands as Commands, ExtensionContext } from "vscode";
+import { commands as Commands, env, ExtensionContext, Uri, window as Window } from "vscode";
 import { activateTreeView, deactivateTreeView } from "./deviceView";
 import { activateLsp, deactivateLsp } from "./lspClient";
 import { activateToitStatusBar, createSetOrgCommand } from "./organization";
@@ -14,10 +14,40 @@ import { createSerialProvision } from "./toitProvision";
 import { createStartSimCommand, createStopSimCommand } from "./toitSimulator";
 import { createUninstallCommand } from "./toitUninstall";
 import { Context, revealDevice } from "./utils";
+import cp = require("child_process");
 
-export function activate(extContext: ExtensionContext): void {
-  Commands.executeCommand("setContext", "toit.extensionActive", true);
+async function checkToitCLI(ctx: Context): Promise<boolean> {
+  return new Promise<boolean>( (resolve) => {
+    cp.execFile(ctx.toitExec, [ "version", "-o", "json" ], (err) => {
+      if (err?.code === "ENOENT") {
+        return resolve(false);
+      }
+      resolve(true);
+    });
+  });
+}
+
+async function missingCLIPrompt() {
+  const installAction = "Install";
+  const settingsAction = "Update settings";
+  const action = await Window.showErrorMessage("Could not find the `toit` executable. Please make sure `toit` is installed and set the toit.Path setting to the executable (reload the window to activate the extension).", installAction, settingsAction);
+  if (action === installAction) {
+    env.openExternal(Uri.parse("https://docs.toit.io/getstarted/installation"));
+  } else if (action === settingsAction) {
+    Commands.executeCommand( "workbench.action.openSettings", "toit.Path" );
+  }
+}
+
+export async function activate(extContext: ExtensionContext): Promise<void> {
   const ctx = new Context();
+  const toitCLI = await checkToitCLI(ctx);
+  if (!toitCLI) {
+    missingCLIPrompt();
+    return;
+  }
+
+  Commands.executeCommand("setContext", "toit.extensionActive", true);
+
   activateTreeView(ctx);
   activateSerialView(ctx);
 
