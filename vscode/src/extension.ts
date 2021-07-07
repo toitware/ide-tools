3c90a8781227af9143654d6380ceaf1112a934b5
Lau Skorstengaard <lau@toitware.com>
2021-07-07 11:43:01 +0200

    Convert version to string

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -21,7 +21,8 @@ const MIN_TOIT_VERSION = "1.7.0";
 
 async function checkToitCLI(ctx: Context): Promise<boolean> {
   try {
-    const version = clean(cp.execFileSync(ctx.toitExec, [ "version", "-o", "short" ]));
+    const v = cp.execFileSync(ctx.toitExec, [ "version", "-o", "short" ]);
+    const version = clean(v.toString());
     if (!version || gt(MIN_TOIT_VERSION, version)) {
       invalidCLIVersionPrompt(ctx, version);
       return false;
