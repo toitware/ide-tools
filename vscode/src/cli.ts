e87828ddbfa4d2a30d283b27b2414e603a35cd2a
Jesper Lindstrøm Nielsen <jesper.jln@gmail.com>
2021-11-02 21:12:25 +0100

    added 'TOIT_EXTERNAL_APPLICATION' env to toit calls

--- vscode/src/cli.ts
+++ vscode/src/cli.ts
@@ -1,21 +1,30 @@
 
-import cp = require("child_process");
+import * as cp from "child_process";
 import { promisify } from "util";
 import { Context } from "./utils";
+const pjson = require('./package.json');
 const execFile = promisify(cp.execFile);
 
+const env = {
+  ...process.env,
+  "TOIT_EXTERNAL_APPLICATION": "Toit-VSCodeExtension" + (pjson.version ? "/" + pjson.version : ""),
+}
+
 export function toitExecFileSync(ctx: Context, ...args: string[]): string {
-  return cp.execFileSync(ctx.toitExec, args, {"encoding": "utf8"});
+  return cp.execFileSync(ctx.toitExec, args, {
+    "encoding": "utf8",
+    "env": env,
+  });
 }
 
 export function toitExecFilePromise(ctx: Context, ...args: string[]): Promise<{stdout: string, stderr: string}> {
-  return execFile(ctx.toitExec, args);
+  return execFile(ctx.toitExec, args, {"env": env});
 }
 
 export function toitExecFile(ctx: Context, ...args: string[]): cp.ChildProcess {
-  return cp.execFile(ctx.toitExec, args);
+  return cp.execFile(ctx.toitExec, args, {"env": env});
 }
 
 export function toitSpawn(ctx: Context, ...args: string[]): void {
-  cp.spawn(ctx.toitExec, args);
+  cp.spawn(ctx.toitExec, args, {"env": env});
 }
